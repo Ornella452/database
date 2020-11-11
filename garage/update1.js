776aa09b651ec40f7b774da248a636b7d0c0c544
ourfi <Ornella93@outlook.fr>
2020-11-12 00:00:32 +0100

    jours 5 no-sql finish en comprrenant la méthode findByIdAndUpdate

--- garage/update1.js
+++ garage/update1.js
@@ -1,25 +1,41 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/garage', 
-{useNewUrlParser: true,
-    useUnifiedTopology: true
-}
+mongoose.connect('mongodb://localhost:27017/garage',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
 ).catch(error => console.log(error));
 
 const Car = new mongoose.Schema({
-    brand:  String,
+    brand: String,
     model: String,
-    year:  Number,
+    year: Number,
     created: Date
-  });
+});
 
-  const VoitureModel = mongoose.model('Voiture', Car);
-  
-  VoitureModel.update({year:2000}, function (err, result) { 
-    if (err){ 
-        console.log(err) 
-    }else{ 
-        console.log("Result :", result)  
-    } 
+const VoitureModel = mongoose.model('Voiture', Car);
+/*
+VoitureModel.update({year:2020}, function (err, result) { 
+  if (err){ 
+      console.log(err) 
+  }else{ 
+      console.log("Result :", result)  
+  } 
 }); 
+*/
+
+/// pour modifier quelque chose en spécifiant la donnée qu'on veut modifier grace l'id
+VoitureModel.findByIdAndUpdate("5fab072d2046723efc495d55", { year: 2020 },
+    function (err, docs) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("Updated User : ", docs);
+        }
+    });
+
+
+
 
 
